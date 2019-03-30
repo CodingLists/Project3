e8506afee5eea6e6850a744ece1e8a1f44fdce2b
Jacob A. Shumard <jacobshu62@ou.edu>
2019-03-30 17:59:22 -0500

    Unfinished method

--- StationLexicographical.java
+++ StationLexicographical.java
@@ -1,13 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
-public class StationLexicographical
+public class StationLexicographical extends MesoSortedAbstract
 {
 
 	private HashMap<String, Integer> asciiVal;
 	
+	
 	public StationLexicographical(HashMap<String, Integer> asciiVal) 
 	{
 		this.asciiVal = asciiVal;
+		
+		ArrayList<String> list = new ArrayList<String>();
+		Set<String> set = asciiVal.keySet();
+		
+		for(String key : set)
+		{
+			list.add(key);
+		}
+		
+		
+		Collections.sort(list);
+		
+		
 	}
+
+
+	
 	
 }
