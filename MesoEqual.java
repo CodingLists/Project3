7f2e0ee2b574b7c592851797e016b2fb73000264
Jacob A. Shumard <jacobshu62@ou.edu>
2019-03-30 17:35:25 -0500

    added cal method

--- MesoEqual.java
+++ MesoEqual.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class MesoEqual
@@ -10,10 +13,32 @@ public class MesoEqual
 		this.stID = stID;
 	}
 
-	public HashMap<String, Integer> calAsciiEqual() 
+	public HashMap<String, Integer> calAsciiEqual() throws IOException 
 	{
+		MesoAscii here = new MesoAscii(new MesoStation(this.stID));
+		HashMap<String, Integer> hash = new HashMap<String, Integer>();
+		hash.put(this.stID, here.calAverage());
 		
-		return null;
+		BufferedReader br = new BufferedReader(new FileReader("Mesonet.txt"));
+    	br.readLine(); 
+    	br.readLine();
+    	br.readLine();
+    	br.readLine();
+    	
+    	while(br.ready())
+    	{
+	    	String[] tokens = br.readLine().split("  ");
+	    	String strg = tokens[1];
+	    	MesoAscii there = new MesoAscii(new MesoStation(strg));
+	    	if(here.calAverage() == there.calAverage())
+	    	{
+	    		hash.put(strg, there.calAverage());
+	    	}
+    	}
+    	
+    	br.close();
+    	return hash;
+    	
 	}
 	
 }
