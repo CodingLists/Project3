af0fbd774dece8e17ab232e9fa41b5d82d82b8a5
Jacob A. Shumard <jacobshu62@ou.edu>
2019-03-30 18:27:38 -0500

    fixed error

--- MesoAscii.java
+++ MesoAscii.java
@@ -17,7 +17,7 @@ public class MesoAscii extends MesoAsciiAbstract
 		int char3 = (int)this.meso.getStID().charAt(2);
 		int char4 = (int)this.meso.getStID().charAt(3);
 		
-		int average = (char1 + char2 + char3 + char4)/4;
+		int average = (int)(Math.round((char1 + char2 + char3 + char4)/4.0));
 		return average;
 	}
 	
