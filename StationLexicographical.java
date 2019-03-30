583b04ee3679f06f1e05a4a1940792ac98bd6419
Jacob A. Shumard <jacobshu62@ou.edu>
2019-03-30 17:41:27 -0500

    added constructor

--- StationLexicographical.java
+++ StationLexicographical.java
@@ -1,4 +1,13 @@
+import java.util.HashMap;
+
 public class StationLexicographical
 {
+
+	private HashMap<String, Integer> asciiVal;
+	
+	public StationLexicographical(HashMap<String, Integer> asciiVal) 
+	{
+		this.asciiVal = asciiVal;
+	}
 	
 }
